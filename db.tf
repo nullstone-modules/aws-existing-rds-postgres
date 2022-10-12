2ebe4e970e47e61d7a30340a0f9f27b0511dcaf7
Brad Sickles <brad.sickles@gmail.com>
2022-10-12 16:49:46 -0400

    Added db instance

---
+++ db.tf
@@ -0,0 +1,10 @@
+data "aws_db_instance" "this" {
+  db_instance_identifier = var.db_identifier
+}
+
+locals {
+  db_security_group_id         = data.aws_db_instance.this.db_security_groups
+  enabled_logs                 = data.aws_db_instance.this.enabled_cloudwatch_logs_exports
+  cloudwatch_log_group_pg      = contains(local.enabled_logs, "postgresql") ? "/aws/rds/instance/${var.db_identifier}/postgresql" : ""
+  cloudwatch_log_group_upgrade = contains(local.enabled_logs, "upgrade") ? "/aws/rds/instance/${var.db_identifier}/upgrade" : ""
+}
